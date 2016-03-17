bb55f96e08c8aa2c6328e6f95a9c5eb55eb5e156
Brandon Greenwell <greenwell.brandon@gmail.com>
2016-03-17 15:36:40 -0400

    new cprint function for "copy-print"

---
+++ R/cprint.R
@@ -0,0 +1,44 @@
+#' Copy Print
+#'
+#' The cprint function provides a capability to print common R data structures 
+#' in a form which can be used as input to the interpreter. 
+#'
+#' @param x An appropriate R object.
+#' @param ... Additional optional arguments. Currently ignored.
+#' @rdname cprint
+#' @export
+#' @examples 
+#' cprint(iris)  # output can easily be copied into an RStudio script file
+cprint <- function(x, ...) {
+  UseMethod("cprint")
+}
+
+
+#' @rdname cprint
+#' @export
+cprint.numeric <- function(x, ...) {
+  string <- paste0("c(", paste0(x, collapse = ", "), ")")
+  cat(string, "\n")
+}
+
+
+#' @rdname cprint
+#' @export
+cprint.character <- function(x, ...) {
+  string <- paste0("c(", paste0("'", x, "'", collapse = ", "), ")")
+  cat(string, "\n")
+}
+
+
+#' @rdname cprint
+#' @export
+cprint.data.frame <- function(x, ...) {
+  cnames <- names(x)
+  cat("data.frame(", "\n", ...)
+  for (i in seq_len(length(cnames) - 1)) {
+    cat(cnames[i], " = ", paste0("c(", paste0("'", x[[i]], "'", collapse = ", "), "),"), "\n", ...)
+  }
+  cat(cnames[length(cnames)], " = ", paste0("c(", paste0("'", x[[length(cnames)]], "'", collapse = ", "), ")"), "\n", ...)
+  cat(")", "\n", ...)
+}
+
