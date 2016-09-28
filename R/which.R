edc900b0bf58faa7e381175c375b330179a92427
Brandon Greenwell <greenwell.brandon@gmail.com>
2016-09-28 12:47:34 -0400

    Create which.R

---
+++ R/which.R
@@ -0,0 +1,46 @@
+which.numeric <- function(x, names = TRUE) {
+  pos <- which(sapply(x, class) == "numeric")
+  if (names) {
+    names(x)[pos]
+  } else {
+    pos
+  }
+}
+
+
+which.factor <- function(x, names = TRUE) {
+  pos <- which(sapply(x, class) == "factor")
+  if (names) {
+    names(x)[pos]
+  } else {
+    pos
+  }
+}
+
+
+which.character <- function(x, names = TRUE) {
+  pos <- which(sapply(x, class) == "character")
+  if (names) {
+    names(x)[pos]
+  } else {
+    pos
+  }
+}
+
+
+which.integer <- function(x, names = TRUE) {
+  pos <- which(sapply(x, class) == "integer")
+  if (names) {
+    names(x)[pos]
+  } else {
+    pos
+  }
+}
+
+
+varTypes <- function(x) {
+  types <- sapply(x, class))
+  lapply(sort(unique(types)), function(y) {
+    
+  })
+}
